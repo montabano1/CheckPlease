69afb0c2f9275602e3b0baf2f2bc58e434aa2ee3
montabano1@gmail.com <appacademy@Apps-Mac-mini-2.local>
2018-07-12 17:48:28 -0400

    Work on restaurant show, finishing map next

--- frontend/components/restaurants/restaurant_show.jsx
+++ frontend/components/restaurants/restaurant_show.jsx
@@ -62,6 +62,7 @@ class RestaurantShow extends React.Component {
             </picture>
 
           </picture>
+
           <main className='restaurant-info-container'>
             <h1 className='restaurant-show-name'>{restaurant.name}</h1>
             <div className="restaurant-show-info">
@@ -70,7 +71,72 @@ class RestaurantShow extends React.Component {
                 &nbsp;&nbsp;
                 <strong>Price - {restaurant.price}</strong>
               </div>
-              Description - {restaurant.description}
+                <section className='restaurant-description'>
+                  Description - {restaurant.description}
+                </section>
+                <div className='restaurant-specifics'>
+                  <section className='specifics'>
+                    <span className='specifics-title'> Private Dining </span>
+                  </section>
+                  <section className='specifics'>
+                    <span className='specifics-title'> Dining Style </span>
+                    <span className='specifics-description'>Casual Elegant</span>
+                  </section>
+                  <section className='specifics'>
+                    <span className='specifics-title'> Cuisine </span>
+                    <span className='specifics-description'>{restaurant.cuisine}</span>
+                  </section>
+                  <section className='specifics-hours'>
+                    <span className='specifics-title'> Hours of operation </span>
+                      <span className='specifics-description'>
+                        Dinner: Monday - Wednesday 5:30pm - 10:30pm, Thursday 5:30pm - 11:00pm, Friday - Saturday 5:30pm - 11:30pm, Sunday 5:30pm - 10:00pm,
+                        Lunch Monday - Friday 12:00pm - 3:00pm;
+                        Brunch Saturday -Sunday 11:00am - 3pm
+                      </span>
+                  </section>
+                  <section className='specifics'>
+                    <span className='specifics-title'> Phone number </span>
+                    <span className='specifics-description'>(555) 475-5829</span>
+                  </section>
+                  <section className='specifics'>
+                    <span className='specifics-title'> Website </span>
+                    <span className='specifics-description'>{restaurant.menu_url}</span>
+                  </section>
+                  <section className='specifics'>
+                    <span className='specifics-title'> Payment options </span>
+                    <span className='specifics-description'>AMEX, Discover, Mastercard, Visa</span>
+                  </section>
+                  <section className='specifics'>
+                    <span className='specifics-title'> Dress code </span>
+                    <span className='specifics-description'>Casual Dress</span>
+                  </section>
+                  <section className='map'>
+                    map
+                  </section>
+
+                  <section className='specifics-address'>35 East 1838th Street New York, NY 10003</section>
+                    <section className='specifics2'>
+                      <span className='specifics-title'> Neighborhood </span>
+                      <span className='specifics-description'>Gramercy/ Flatiron</span>
+                    </section>
+                    <section className='specifics2'>
+                      <span className='specifics-title'> Cross street </span>
+                      <span className='specifics-description'>Broadway & Park Avenue South</span>
+                    </section>
+                    <section className='specifics2'>
+                      <span className='specifics-title'> Parking details </span>
+                      <span className='specifics-description'>No Parking</span>
+                    </section>
+                    <section className='specifics2'>
+                      <span className='specifics-title'> Public transit </span>
+                      <span className='specifics-description'>L,N,Q,R,W,4,5,6 to Union Square</span>
+                    </section>
+                    <section className='specifics2'>
+                      <span className='specifics-title'> Additional </span>
+                      <span className='specifics-description'>Bar Dining, Bar/Lounge, Beer</span>
+                    </section>
+
+                </div>
             </div>
           </main>
         </div>
