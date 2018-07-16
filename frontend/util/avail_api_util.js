887d57346cb01708ecb7d0bb90fea290c038c8ff
montabano1 <montabano1@gmail.com>
2018-07-15 21:15:32 -0400

    Fix syntax errors2

--- frontend/util/avail_api_util.js
+++ frontend/util/avail_api_util.js
@@ -2,14 +2,14 @@ export const fetchAvails = () => {
   return $.ajax ({
     method: 'GET',
     url: '/api/avails'
-  });
+  })
 };
 
 export const fetchAvail = (id) => {
   return $.ajax ({
     method: 'GET',
     url: `/api/avails/${id}`
-  });
+  })
 };
 
 export const createAvail = (avail) => {
@@ -17,11 +17,11 @@ export const createAvail = (avail) => {
     method: 'POST',
     url: `/api/avails`,
     data: { avail }
-  });
+  })
 };
 export const deleteAvail = id => (
   return $.ajax({
     method: 'DELETE'
     url: `api/avails/${id}`,
-  });
-);
+  })
+};
